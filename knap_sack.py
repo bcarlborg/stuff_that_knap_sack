8907359c5d23f4b1c40dfa129cf7221466a6f2a1
Beau Carlborg <bcarlborg@gmail.com>
2018-04-08 12:00:39 -0600

    built basic working draft

--- knap_sack.py
+++ knap_sack.py
@@ -8,6 +8,26 @@ def generate_random_item_list(list_length, max_weight, max_value):
 	return item_list
 
 
+def find_best_packing( input_list, max_weight ):
+	
+	memoized_list = construct_item_weight_array( item_list = input_list, max_weight = max_weight ):
+	current_node = 1
+	for node in input_list:
+		current_weight = 0
+		for weight in range( 0, ( max_weight + 1 ) ):
+			value_without_curr_node = memoized_list[ (current_node - 1), current_weight ]
+			if ( node.weight < current_weight ):
+				memoized_list[ current_node, current_weight ] = value_without_curr_node
+			else:
+				value_with_curr_node = memoized_list[ (current_node - 1), (current_weight - node.weight) ] + node.value 
+				memoized_list[current_node, current_weight] = value_with_current_node if (value_with_current_node > value without current_node) else value_without_current_node 
+
+
+def construct_item_weight_array( item_list, max_weight ):
+	output_array = []
+	for item in range( 0, len( item_list ) + 1 ):
+		output_array.append( [0] * (max_weight + 1) )
+	return output_array
 
 
 class knap_sack_item():
--- test_knap_sack.py
+++ test_knap_sack.py
@@ -12,10 +12,20 @@ class generate_random_item_list_test( unittest.TestCase ):
 	def test_empty_list_length( self ):
 		"""tests an empty input list"""
 		item_list = knap_sack.generate_random_item_list( list_length = 0, max_weight = 10, max_value = 10 )
-		self.assertEqual( 0, len(item_list) )
+		self.assertEqual( 0, len( item_list ) )
 
+class construct_item_weight_array_test( unittest.TestCase ):
+	"""test for the construct_item_weight_array_function"""
 
+	def setUp( self ):
+		self.input_items = [ knap_sack.knap_sack_item( weight = 10, value = 10 ), knap_sack.knap_sack_item( weight = 8, value = 8 ), knap_sack.knap_sack_item( weight = 6, value = 6) ]
+		self.output_array = knap_sack.construct_item_weight_array( item_list = self.input_items, max_weight = 14 )
 
+	def test_array_list_length( self ):
+		self.assertEqual( 4, len( self.output_array ) )
+
+	def test_zeroith_list_element_for_zeros( self ):
+		self.assertEqual( ( [0] * ( 14 + 1 ) ), self.output_array[0] ) 
 
 if __name__ == '__main__':
 	unittest.main()
